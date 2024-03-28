c68655ac54614bd2f139a6958920e3afbf77bd8b
Cili <93920033+Anuyil-cili@users.noreply.github.com>
2024-03-28 01:43:09 +0100

    Update EthUDP.h

--- EthUDP.h
+++ EthUDP.h
@@ -9,7 +9,7 @@ public:
     ~EthUDP();
 
     nsapi_error_t initialize(const char* ip, const char* netmask, const char* gateway, int port);
-    nsapi_size_or_error_t send(const char* data, SocketAddress sendAddr);
+    nsapi_size_or_error_t send(const char* data, nsapi_size_t size, SocketAddress sendAddr);
     nsapi_size_or_error_t send(const char* data, nsapi_size_t size, const char* addr, uint16_t port);
     nsapi_size_or_error_t receive(char* buffer, nsapi_size_t size, uint32_t timeout); //ricevo da chiunque collegato sulla porta
     nsapi_size_or_error_t receiveFrom(char* buffer, nsapi_size_t size, const char* sender_ip, int port, uint32_t timeout); //ricevo da un indirizzo ip specifico
